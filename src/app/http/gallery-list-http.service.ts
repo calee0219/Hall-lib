ecfcc620795280245763fac3c2e5c18de9d84845
Gavin Lee <sz110010@gmail.com>
2016-12-19 14:52:14 +0800

    get mData

--- src/app/http/gallery-list-http.service.ts
+++ src/app/http/gallery-list-http.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
+import { Card } from '../type/card';
+import { CARDS } from './mData';
+
 @Injectable()
 export class GalleryListHttpService {
 
   constructor(private http: Http) { }
+  
+  getmData(): Card[] {
+    return CARDS;
+  }
 
   getData() {
     this.http.get('140.113.66.249:65534/getinfo')
