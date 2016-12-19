2daf87a0eaafc1a0a4ef4dff7b5bf56cabf3577d
Gavin Lee <sz110010@gmail.com>
2016-12-19 14:43:22 +0800

    Create mData.ts

---
+++ src/app/http/mData.ts
@@ -0,0 +1,17 @@
+import { Card } from '../type/card'
+
+const CARDS: Card[] = [
+  {"Title":"夏 / 流 - 第49屆期中作品展","Serial":"583299f0d7d3cVmNLLtM3Za","Cover":"http://140.113.66.249:65534/data/583299f0d7d3cVmNLLtM3Za/file_58329b667111b.jpg","Begin":"2016-11-04","End":"2016-11-29"},
+  {"Title":"海外祭","Serial":"583b90b65d01a9eHBkb5cvH","Cover":"http://140.113.66.249:65534/data/583b90b65d01a9eHBkb5cvH/file_583b921270fda.jpg","Begin":"2016-11-01","End":"2016-11-30"},
+  {"Title":"凝香：王力紅工筆花鳥師生畫展","Serial":"5808648e8cd31i8O9wt488T","Cover":"http://140.113.66.249:65534/data/5808648e8cd31i8O9wt488T/file_5808652295b79.jpg","Begin":"2016-10-01","End":"2016-10-31"},
+  {"Title":"柯瑩玲醫師與藝友拼布聯展 ","Serial":"57cd225a4608bANN8AOrneA","Cover":"http://140.113.66.249:65534/data/57cd225a4608bANN8AOrneA/file_57cd22f7ca259.jpg","Begin":"2016-09-02","End":"2016-09-29"},
+  {"Title":"璀璨之美─恆星學坊學員作品展","Serial":"57aa889d41783DuL8kM5BKs","Cover":"http://140.113.66.249:65534/data/57aa889d41783DuL8kM5BKs/file_57aa891bc5d53.jpg","Begin":"2016-08-02","End":"2016-08-30"},
+  {"Title":"2016交大國畫社師生聯展","Serial":"5784aa9745a8dyFYX8dd0Rk","Cover":"http://140.113.66.249:65534/data/5784aa9745a8dyFYX8dd0Rk/file_5784aaf46549e.jpg","Begin":"2016-07-01","End":"2016-07-30"},
+  {"Title":"觀物有情-九彌個展 ","Serial":"5757c96f3be2bLqzR4spbv4","Cover":"http://140.113.66.249:65534/data/5757c96f3be2bLqzR4spbv4/file_5757ca0cdd71c.jpg","Begin":"2016-06-01","End":"2016-06-30"},
+  {"Title":"以愛為名  公益藝文展","Serial":"5732c86fc4fd3CcyiOjlHGe","Cover":"http://140.113.66.249:65534/data/5732c86fc4fd3CcyiOjlHGe/file_5732c9059c9f4.jpg","Begin":"2016-05-01","End":"2016-05-31"},
+  {"Title":"交大歷任校長肖像油畫展","Serial":"5732c600ca4c4wVOwlA9Frz","Cover":"http://140.113.66.249:65534/data/5732c600ca4c4wVOwlA9Frz/file_5732c6db86f62.jpg","Begin":"2016-04-07","End":"2016-04-27"},
+  {"Title":"交大雙甲子校慶特展- 典藏時代記憶","Serial":"571590e976161wZTFP2cIno","Cover":"http://140.113.66.249:65534/data/571590e976161wZTFP2cIno/file_5715a844c2cef.jpg","Begin":"2016-03-23","End":"2016-04-27"},
+  {"Title":"不丸美藥局","Serial":"5726fa38d0b902cpXGKvT9L","Cover":"http://140.113.66.249:65534/data/5726fa38d0b902cpXGKvT9L/file_5726fb4b476b9.jpg","Begin":"2016-03-21","End":"2016-04-07"},
+  {"Title":"招財進寶 富貴封侯","Serial":"56d795e7ab680qPNUt5uF4K","Cover":"http://140.113.66.249:65534/data/56d795e7ab680qPNUt5uF4K/file_56d796c95b186.jpg","Begin":"2016-02-17","End":"2016-03-14"},
+  {"Title":"莊純巧作品展","Serial":"568f5cfaeacda9mEsyxm5ty","Cover":"http://140.113.66.249:65534/data/568f5cfaeacda9mEsyxm5ty/file_568f5e95d4024.jpg","Begin":"2016-01-04","End":"2016-01-30"}
+];
